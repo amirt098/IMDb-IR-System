465aee20739112db511e3a080e316df7b51b91ea
amir tohidi <amir0tohidi@gmail.com>
2024-06-25 11:43:02 +0330

    add rag and BertFinetuner_mask

--- Logic/core/finetuner/BertFinetuner_mask.py
+++ Logic/core/finetuner/BertFinetuner_mask.py
@@ -1,3 +1,25 @@
+import torch
+import json
+import numpy as np
+
+
+import wandb
+
+from torch.utils.data import Dataset
+from collections import Counter
+
+from sklearn.model_selection import train_test_split
+from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments, EvalPrediction
+
+from transformers import default_data_collator
+import evaluate
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+WAND_KEY = 'a965e082ae3823a8a3d8d30404eb4953471a51c9'
+
 class BERTFinetuner:
     """
     A class for fine-tuning the BERT model on a movie genre classification task.
@@ -11,19 +33,69 @@ class BERTFinetuner:
             file_path (str): The path to the JSON file containing the dataset.
             top_n_genres (int): The number of top genres to consider.
         """
-        # TODO: Implement initialization logic
+        #  Implement initialization logic
+        self.file_path = file_path
+        self.top_n_genres = top_n_genres
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.model = None
+        self.encodings = None
+        self.labels = None
+        self.genre_to_label = {}
+        self.label_to_genre = {}
+        self.train_dataset = None
+        self.eval_dataset = None
+        self.test_dataset = None
 
     def load_dataset(self):
         """
         Load the dataset from the JSON file.
         """
-        # TODO: Implement dataset loading logic
+        # Implement dataset loading logic
+        with open(self.file_path, 'r') as f:
+            self.data = json.load(f)
+
+        if self.data is not None:
+            logger.info("Loaded")
+        else:
+            logger.info("NOT Loaded")
+            raise Exception("Dataset not loaded")
 
     def preprocess_genre_distribution(self):
         """
         Preprocess the dataset by filtering for the top n genres
         """
-        # TODO: Implement genre filtering and visualization logic
+        # Implement genre filtering and visualization logic
+        summaries = []
+        genres = []
+
+        for movie_id, movie_data in self.data.items():
+            if 'first_page_summary' in movie_data and 'genres' in movie_data and movie_data['genres']:
+
+                summary = movie_data['first_page_summary']
+                genre = movie_data['genres'][0]
+                if summary:
+                    summaries.append(summary)
+                    genres.append(genre)
+                else:
+                    logging.debug(f"null summary  for movie_id {movie_id}: {type(summary)}")
+
+        logging.debug(f"# summaries collected: {len(summaries)}")
+        logging.debug(f"# genres collected: {len(genres)}")
+
+        genre_counts = Counter(genres)
+        top_genres = [genre for genre, count in genre_counts.most_common(self.top_n_genres)]
+
+        filtered_data = [(s, g) for s, g in zip(summaries, genres) if g in top_genres]
+        summaries, genres = zip(*filtered_data)
+
+        logging.debug(f"Summaries: {summaries[:5]}")
+        logging.debug(f"Genres: {genres[:5]}")
+
+        self.genre_to_label = {genre: idx for idx, genre in enumerate(top_genres)}
+        self.label_to_genre = {idx: genre for genre, idx in self.genre_to_label.items()}
+        self.labels = [self.genre_to_label[genre] for genre in genres]
+
+        self.encodings = self.tokenizer(list(summaries), truncation=True, padding=True)
 
     def split_dataset(self, test_size=0.3, val_size=0.5):
         """
@@ -33,7 +105,36 @@ class BERTFinetuner:
             test_size (float): The proportion of the dataset to include in the test split.
             val_size (float): The proportion of the dataset to include in the validation split.
         """
-        # TODO: Implement dataset splitting logic
+        # Implement dataset splitting logic
+
+        summaries = list(self.encodings['input_ids'])
+        labels = list(self.labels)
+
+        # Debugging the lengths of summaries and labels
+        logging.debug(f"Summaries length: {len(summaries)}")
+        logging.debug(f"Labels length: {len(labels)}")
+
+        # Ensure the lengths are consistent
+        assert len(summaries) == len(labels), "Summaries and labels must have the same length"
+
+        # Split the data into training and test sets
+        train_texts, test_texts, train_labels, test_labels = train_test_split(
+            summaries, labels, test_size=test_size, random_state=42
+        )
+
+        # Further split the training data into training and validation sets
+        train_texts, val_texts, train_labels, val_labels = train_test_split(
+            train_texts, train_labels, test_size=val_size, random_state=42
+        )
+
+        # Creating datasets
+        self.train_dataset = self.create_dataset({'input_ids': train_texts}, train_labels)
+        self.eval_dataset = self.create_dataset({'input_ids': val_texts}, val_labels)
+        self.test_dataset = self.create_dataset({'input_ids': test_texts}, test_labels)
+
+        logging.debug(f"train dataset size: {len(self.train_dataset)}")
+        logging.debug(f"validation dataset size: {len(self.eval_dataset)}")
+        logging.debug(f"test dataset size: {len(self.test_dataset)}")
 
     def create_dataset(self, encodings, labels):
         """
@@ -46,7 +147,8 @@ class BERTFinetuner:
         Returns:
             IMDbDataset: A PyTorch dataset object.
         """
-        # TODO: Implement dataset creation logic
+        # Implement dataset creation logic
+        return IMDbDataset(encodings, labels)
 
     def fine_tune_bert(self, epochs=5, batch_size=16, warmup_steps=500, weight_decay=0.01):
         """
@@ -58,7 +160,35 @@ class BERTFinetuner:
             warmup_steps (int): The number of warmup steps for the learning rate scheduler.
             weight_decay (float): The strength of weight decay regularization.
         """
-        # TODO: Implement BERT fine-tuning logic
+        # Implement BERT fine-tuning logic
+        wandb.login(key=WAND_KEY)
+
+        wandb.login(key=WAND_KEY)
+        num_labels = len(self.genre_to_label)
+        self.model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=num_labels)
+
+        training_args = TrainingArguments(
+            output_dir='./results',
+            num_train_epochs=epochs,
+            per_device_train_batch_size=batch_size,
+            per_device_eval_batch_size=batch_size,
+            warmup_steps=warmup_steps,
+            weight_decay=weight_decay,
+            logging_dir='./logs',
+            evaluation_strategy="epoch"
+        )
+
+        trainer = Trainer(
+            model=self.model,
+            args=training_args,
+            train_dataset=self.train_dataset,
+            eval_dataset=self.eval_dataset,
+            tokenizer=self.tokenizer,
+            data_collator=default_data_collator,
+            compute_metrics=self.compute_metrics
+        )
+
+        trainer.train()
 
     def compute_metrics(self, pred):
         """
@@ -70,13 +200,40 @@ class BERTFinetuner:
         Returns:
             dict: A dictionary containing the computed metrics.
         """
-        # TODO: Implement metric computation logic
+        #  Implement metric computation logic
+        metric = evaluate.load("accuracy")
+        logits, labels = pred
+
+        predictions = np.argmax(logits, axis=-1)
+        accuracy = metric.compute(predictions=predictions, references=labels)
+
+        precision = evaluate.load("precision")
+        recall = evaluate.load("recall")
+        f1 = evaluate.load("f1")
+
+        precision_score = precision.compute(predictions=predictions, references=labels, average="weighted")
+        recall_score = recall.compute(predictions=predictions, references=labels, average="weighted")
+        f1_score = f1.compute(predictions=predictions, references=labels, average="weighted")
+
+        return {
+            'accuracy': accuracy['accuracy'],
+            'precision': precision_score['precision'],
+            'recall': recall_score['recall'],
+            'f1': f1_score['f1']
+        }
 
     def evaluate_model(self):
         """
         Evaluate the fine-tuned model on the test set.
         """
-        # TODO: Implement model evaluation logic
+        # Implement model evaluation logic
+        trainer = Trainer(model=self.model)
+        eval_results = trainer.evaluate(self.test_dataset)
+        print(eval_results)
+        # print(f"test Accuracy: {eval_results['eval_accuracy']}")
+        # print(f"test Precision: {eval_results['eval_precision']}")
+        # print(f"test Recall: {eval_results['eval_recall']}")
+        # print(f"test F1 Score: {eval_results['eval_f1']}")
 
     def save_model(self, model_name):
         """
@@ -85,7 +242,10 @@ class BERTFinetuner:
         Args:
             model_name (str): The name of the model on the Hugging Face Hub.
         """
-        # TODO: Implement model saving logic
+        #  Implement model saving logic
+        self.model.save_pretrained(model_name)
+        self.tokenizer.save_pretrained(model_name)
+
 
 class IMDbDataset(torch.utils.data.Dataset):
     """
@@ -100,7 +260,9 @@ class IMDbDataset(torch.utils.data.Dataset):
             encodings (dict): The tokenized input encodings.
             labels (list): The corresponding labels.
         """
-        # TODO: Implement initialization logic
+        # Implement initialization logic
+        self.encodings = encodings
+        self.labels = labels
 
     def __getitem__(self, idx):
         """
@@ -112,7 +274,11 @@ class IMDbDataset(torch.utils.data.Dataset):
         Returns:
             dict: A dictionary containing the input encodings and labels.
         """
-        # TODO: Implement item retrieval logic
+        # Implement item retrieval logic
+
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
 
     def __len__(self):
         """
@@ -121,4 +287,5 @@ class IMDbDataset(torch.utils.data.Dataset):
         Returns:
             int: The number of items in the dataset.
         """
-        # TODO: Implement length computation logic
+        # Implement length computation logic
+        return len(self.labels)
--- Logic/core/finetuner/main.py
+++ Logic/core/finetuner/main.py
@@ -1,5 +1,7 @@
+from BertFinetuner_mask import BERTFinetuner
+
 # Instantiate the class
-bert_finetuner = BERTFinetuner('path/to/your/file.json', top_n_genres=5)
+bert_finetuner = BERTFinetuner('documents.json', top_n_genres=5)
 
 # Load the dataset
 bert_finetuner.load_dataset()
@@ -13,8 +15,68 @@ bert_finetuner.split_dataset()
 # Fine-tune BERT model
 bert_finetuner.fine_tune_bert()
 
+"""
+{'
+ eval_loss': 1.3361676931381226,
+'eval_accuracy': 0.4340681362725451,
+'eval_precision': 0.5026897140194923,
+'eval_recall': 0.4340681362725451,
+'eval_f1': 0.3346449230339283,
+'eval_runtime': 158.3178,
+'eval_samples_per_second': 15.759,
+'eval_steps_per_second': 0.985,
+'epoch': 1.0
+}
+{
+'eval_loss': 1.0868537425994873,
+'eval_accuracy': 0.556312625250501,
+'eval_precision': 0.5623027818640981, 
+'eval_recall': 0.556312625250501, 
+'eval_f1': 0.5566987299182602, 
+'eval_runtime': 314.641, 
+'eval_samples_per_second': 7.93, 
+'eval_steps_per_second': 0.496, 
+'epoch': 2.0
+}
+{'eval_loss': 1.3024715185165405, 
+'eval_accuracy': 0.5290581162324649, 
+'eval_precision': 0.5989382357140508, 
+'eval_recall': 0.5290581162324649, 
+'eval_f1': 0.4902333174587944, 
+'eval_runtime': 158.704, 
+'eval_samples_per_second': 15.721, 
+'eval_steps_per_second': 0.983, 
+'epoch': 3.0}
+{'eval_loss': 1.3117491006851196, 
+'eval_accuracy': 0.5535070140280561, 
+'eval_precision': 0.579463137771552, 
+'eval_recall': 0.5535070140280561, 
+'eval_f1': 0.554481100102594, 
+'eval_runtime': 312.531, 
+'eval_samples_per_second': 7.983, 
+'eval_steps_per_second': 0.499, 
+'epoch': 4.0}
+{'eval_loss': 1.5852760076522827, 
+'eval_accuracy': 0.5727454909819639, 
+'eval_precision': 0.5799105960880501, 
+'eval_recall': 0.5727454909819639, 
+'eval_f1': 0.5746087199761549, 
+'eval_runtime': 356.9751, 
+'eval_samples_per_second': 6.989, 
+'eval_steps_per_second': 0.437, 
+'epoch': 5.0}
+
+{'train_runtime': 2991.5607, 
+'train_samples_per_second': 4.17, 
+'train_steps_per_second': 0.261, 
+'train_loss': 0.865917489467523, 
+'epoch': 5.0}
+"""
+
 # Compute metrics
 bert_finetuner.evaluate_model()
 
+
 # Save the model (optional)
 bert_finetuner.save_model('Movie_Genre_Classifier')
+
